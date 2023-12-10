35716d2099fd0f9d9c13f0e662a95027a8fc5929
Tang Hien Phan <phan05042000@gmail.com>
2023-12-10 18:52:20 +0700

    Add function to check frequent parking number

--- classes/options/parkOption.py
+++ classes/options/parkOption.py
@@ -1,11 +1,13 @@
 from .option import Option
 from datetime import datetime
+from constants.folderNames import *
 
 class ParkOption(Option):
     def __init__(self, car_identity_value) -> None:
         super().__init__(car_identity_value)
         self.arrival_time = input('Please enter the arrival time: ')
         self.frequent_parking_number = input('Please enter the frequent parking number: ')
+        self.check_folder_or_file_exist(park_folder_name)
 
     @property
     def arrival_time(self) -> str:
@@ -16,8 +18,9 @@ class ParkOption(Option):
         valid = False
         while not valid:
             try:
+                value = value + ':00' if value.count(':') < 2 else value
                 self.__arrival_time = datetime.strptime(value, self.time_format)
-                valid= True
+                valid = True
             except Exception as e:
                 print(f'Error message: {e}')
                 value = input('Please enter the arrival time: ')
@@ -28,14 +31,16 @@ class ParkOption(Option):
     
     @frequent_parking_number.setter
     def frequent_parking_number(self, value) -> None:
-        self.__frequent_parking_number = self.modulo_11_check_digit(value)
+        self.__frequent_parking_number = self.check_frequent_parking_number_by_modulo_11(value)
 
-    def modulo_11_check_digit(self, value):
-        if value == '12343':
-            return value
-        return None
+    def check_frequent_parking_number_by_modulo_11(self, value) -> None | str:
+        sum_digits = 0
+        for digit_index, digit in enumerate(value):
+            sum_digits += (int(digit) * (len(value) - digit_index))
+            print(f'{digit} * {(len(value) - digit_index)}')
+        return value if sum_digits % 11 == 0 else None
 
     def execute(self) -> None:
-        f = open(f"./files/park/{self.car_identity}.txt", "w")
-        f.write(f'{self.car_identity} - {self.arrival_time} - {self.frequent_parking_number}')
-        print(f'Finished writing parking file for {self.car_identity}')
+        content = f'{self.car_identity} , {self.arrival_time} , {self.frequent_parking_number}'
+        message = f'Finished writing parking file for {self.car_identity}'
+        self.write_file(content, message, park_folder_name)
